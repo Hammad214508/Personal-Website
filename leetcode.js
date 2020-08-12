58487a62f100baff2d6350dd5cdd256b525988d9
Hammad214508 <hammadmuhammad15@gmail.com>
2020-08-12 21:07:42 +0100

    new leetcode

--- leetcode.js
+++ leetcode.js
@@ -218,8 +218,20 @@ $(document).ready(function(){
 
     August =
     [
-        "Day 1 - Detect Capital"
+        "Day 1 - Detect Capital",
+        "Day 2 - Design HashSet",
+        "Day 3 - Valid Palindrome",
+        "Day 4 - Power of Four",
+        "Day 5 - Add and Search Word - Data structure design",
+        "Day 6 - Find All Duplicates in an Array",
+        "Day 7 - Vertical Order Traversal of a Binary Tree",
+        "Day 8 - Path Sum III",
+        "Day 9 - Rotting Oranges",
+        "Day 10 - Excel Sheet Column Number",
+        "Day 11 - H-Index",
+        "Day 12 - Pascal's Triangle II"
     ]
+    
     $.fn.splitProblems();
 
 });
