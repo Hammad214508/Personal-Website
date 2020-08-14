f94b58de867097cc457c750ae82578ab73f1b77f
Hammad214508 <hammadmuhammad15@gmail.com>
2020-08-14 20:09:36 +0100

    dropdown icon in about me

---
+++ about.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.panel-collapse').on('show.bs.collapse', function () {
+       $(this).siblings('.panel-heading').addClass('active');
+     });
+
+     $('.panel-collapse').on('hide.bs.collapse', function () {
+       $(this).siblings('.panel-heading').removeClass('active');
+     });
+});
