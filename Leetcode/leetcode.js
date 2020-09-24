488966cefcf3b5912d6598400bd69430f59813f0
Hammad214508 <hammadmuhammad15@gmail.com>
2020-09-24 19:54:35 +0100

    updated leetcode september

--- Leetcode/leetcode.js
+++ Leetcode/leetcode.js
@@ -295,11 +295,20 @@ $(document).ready(function(){
     ]
 
     September =
-    [
-        "Day 1 - Largest Time for Given Digits",
+    [   "Day 1 - Largest Time for Given Digits",
         "Day 2 - Contains Duplicate III",
         "Day 3 - Repeated Substring Pattern",
-        "Day 4 - Partition Labels"
+        "Day 4 - Partition Labels",
+        "Day 5 - All Elements in Two Binary Search Trees",
+        "Day 6 - Image Overlap",
+        "Day 7 - Word Pattern",
+        "Day 8 - Sum of Root To Leaf Binary Numbers",
+        "Day 9 - Compare Version Numbers",
+        "Day 10 - Bulls and Cows",
+        "Day 11 - Maximum Product Subarray",
+        "Day 12 - Combination Sum III",
+        "Day 13 - Insert Interval",
+        "Day 14 - House Robber"
     ]
 
     $.fn.splitProblems();
