7d952eaf93c0bb2bfbc23a09df701b734c297ed8
Hammad214508 <hammadmuhammad15@gmail.com>
2021-03-13 20:26:52 +0000

    improved descriptions and separate links for git and hosted

--- Projects/projects.js
+++ Projects/projects.js
@@ -18,29 +18,26 @@ $(document).ready(function(){
      var Projects =
      [
          {
-             title:"Blog App <i class=\"fa fa-github-square\"></i> <i class=\"fa fa-link\" aria-hidden=\"true\"></i>",
+             title:"Blog App <i href='https://github.com/Hammad214508/Blog-Application' class=\"fa fa-github-square git-link \"></i> <i href=\"https://hammad-blog-app.herokuapp.com/\" class=\"fa fa-link link-url\" aria-hidden=\"true\"></i>",
              description:
-                 "I developed a full-featured web application using Django framework and Python. "+
+                 "Developed a full-featured web application using Django framework and Python. "+
                  "It is a social media styled application where different users can post e.g. blog "+
                  "posts, twitter updates and similar posts.",
              tools:"Python, Django, SQL, HTML, CSS",
-             link: "https://github.com/Hammad214508/Blog-Application"
          },
          {
-             title:"Restaurant and Food Recommender <i class=\"fa fa-github-square\"></i>",
+             title:"Restaurant and Food Recommender <i href=\"https://github.com/Hammad214508/Restaurant-And-Food-recommender\" class=\"fa fa-github-square git-link\"></i>",
              description:
                 "Web application that helps users decide where and what to eat using machine learning "+
-                "techniques. Extended so that it can handle a group of users wanting to eat together.",
+                "techniques such as collaborative filtering. Extended to work for a group of users wanting to eat together.",
              tools:"Python, PHP, MySQL, jQuery, HTML & CSS",
-             link: "https://github.com/Hammad214508/Restaurant-And-Food-recommender"
          },
          {
-             title:"Shopping System <i class=\"fa fa-github-square\"></i> <i class=\"fa fa-link\" aria-hidden=\"true\"></i>",
+             title:"Shopping System <i href=\"https://github.com/Hammad214508/Shopping-System\" class=\"fa fa-github-square git-link\"></i> <i href=\"https://eveg-1814453.000webhostapp.com/\" class=\"fa fa-link link-url\" aria-hidden=\"true\"></i>",
              description:
-                "I evaluated and redesigned the user interface for an online shopping system using "+
-                "guideline based evaluation and following Nielsen's Usability Heuristics for User Interface Design",
+                "Evaluated and redesigned the user interface for an online shopping system using "+
+                "guideline based evaluation and following Nielsen's Usability Heuristics for User Interface Design.",
              tools:"PHP, Javascript, HTML, CSS",
-             link: "https://github.com/Hammad214508/Shopping-System"
          },
          {
              title:"Derivative Trade Application",
@@ -49,25 +46,22 @@ $(document).ready(function(){
                 "worked five other course-mates which really enhanced my teamwork skills "+
                 "and leadership skills as I was the project manager.",
              tools:"Python, Django, SQL, HTML, CSS",
-             // link: "derivate_trade.html"
          },
          {
-             title:"Meme Maker <i class=\"fa fa-github-square\"></i>",
+             title:"Meme Maker <i href=\"https://github.com/Hammad214508/Meme-maker\" class=\"fa fa-github-square git-link\"></i>",
              description:
-                "I made a meme maker using Python, given a set of templates and fonts, it"+
-                "can generate memes based on the user input. "+
-                "A modification of this (branch on git) was used in a WhatsApp bot for a friend.",
+                "Command line meme maker using Python, it "+
+                "can generate memes from the user input and was developed to contribute in an open source "+
+                "WhatsApp bot to make and send memes automatically from the messages.",
              tools:"Python",
-             link: "https://github.com/Hammad214508/Meme-maker"
 
          },
          {
-             title:"Hiring Manager Game <i class=\"fa fa-link\" aria-hidden=\"true\"></i>",
+             title:"Hiring Game <i href=\"https://github.com/Hammad214508/Hiring-Game\" class=\"fa fa-github-square git-link\"></i> <i href=\"https://hammad214508.github.io/Hiring-Game/\" class=\"fa fa-link link-url\" aria-hidden=\"true\"></i>",
              description:
                 "A simple clicking game in which you are the hiring manager and you can hire interns, software engineers or senior "+
-                "engineers. Goal is to be efficient managing their cost and value. New competitive mode added",
+                "engineers. Goal is to be efficient managing their cost and value. A competitive mode is also present.",
              tools:"jQuery, HTML, CSS",
-             link: "https://hammad214508.github.io/Hiring-Game/"
          },
          {
              title:"MiniC Compiler",
@@ -76,7 +70,6 @@ $(document).ready(function(){
                 "The main components were lexing, parsing and intermediate code generation. The compiler "+
                 "gave syntax and semantic errors when necessary.",
              tools:"C, C++, LLVM",
-             // link: ""
          },
          {
              title:"Database System for a Department Store",
@@ -84,7 +77,6 @@ $(document).ready(function(){
                  "Using Java along with SQL to create a department store database that was subject to sophisticated queries. "+
                  "Managing the store's inventory, orders, collections, deliveries and staff.",
              tools:"Java, JDBC, SQL",
-             // link:"Projects/database.html"
          },
          {
              title:"Intrusion detection system",
@@ -93,7 +85,6 @@ $(document).ready(function(){
                  "malicious activity detected when the program exits. This includes SYN attack detection, "+
                  "ARP poisoning attack and blacklisted URL detection.",
              tools:"C, Python",
-             // link:"Projects/intrusion_detection.html"
          },
          {
              title:"Resolution proof solver",
@@ -102,7 +93,6 @@ $(document).ready(function(){
                    "CNF and then applies the resolution algorithm letting you know if the given formula is a tautology "+
                    "or not.",
              tools:"Prolog",
-             // link:"Projects/resolution.html"
          },
          {
              title:"PLM compiler",
@@ -111,7 +101,6 @@ $(document).ready(function(){
                    "Then extended the parser to an evaluator,  PLM being a simple programming "+
                    "language that allows users to write code that computes non-negative integers.",
              tools:"JavaCC",
-             // link:"Projects/compiler.html"
          },
          {
              title:"Snake Game for Mathematics Revision",
@@ -119,16 +108,13 @@ $(document).ready(function(){
                   "Developed a snake game (Nokia phone game alike, but with many more features) with A-Level Mathematics questions, "+
                   "making it a fun learning tool for students struggling with this subject.",
              tools:"Python, Pygame, SQL",
-             // link:"Projects/snake_game.html"
          },
          {
              title:"Mastermind",
              description:
-                  "A Haskell console program that simulates a player vs computer game of the board-game Mastermind. The computer was "+
-                  "made to solve a game in 5 moves or less utilising Knuth's mastermind algorithm. This project is my first using a "+
-                  "functional programming language.",
+                  "A Haskell console program that simulates a player vs computer game of the board-game Mastermind. The computer can "+
+                  "solve a game in 5 moves or less utilising Knuth's mastermind algorithm",
              tools:"Haskell",
-             // link:"Projects/mastermind.html"
          },
          {
              title:"Robot Arm",
@@ -137,7 +123,6 @@ $(document).ready(function(){
                  "the ”Tower of Hanoi” problem. In order to solve the problem, a solution algorithm was first designed "+
                  "and then implemented in C.",
              tools:"C",
-             // link:"Projects/robot_arm.html"
          },
          {
              title:"Robot Maze",
@@ -146,7 +131,6 @@ $(document).ready(function(){
                    "and state to determine the direction it will move. Improved it by using data from the first run to traverse the maze more efficiently "+
                    "the second time.",
              tools:"Java",
-             // link:"Projects/robot_maze.html"
          },
          {
              title:"Witter",
@@ -154,7 +138,6 @@ $(document).ready(function(){
                   "Implemented various data structures to store Users, Weets and Followers for a fictional Twitter clone called 'Witter'. They were designed "+
                   "to optimally perform some tasks, e.g. finding the common followers, the trending weets, most popular users.",
              tools:"Java",
-             // link:"Projects/witter.html"
          },
 
      ];
@@ -163,11 +146,11 @@ $(document).ready(function(){
          template =
              "<div id='project_"+i+"' class=\"col-xl-4 col-lg-4 col-md-6 col-sm-6 col-xs-12 bottom-buffer\">"+
              "  <div class=\"white-box\" href=\""+link+"\">"+
-             "      <h3>"+title+"</h3>"+
+             "      <h3 style=\"margin-top:10px;\">"+title+"</h3>"+
              "      <p style=\"height:6.9em; overflow-y: scroll;\">"+
              "           "+description+""+
              "      </p>"+
-             "      <div class=\"tools mt-1\" style='position: absolute; bottom: 10px;'>"+
+             "      <div class=\"tools\" style='position: absolute; bottom: 10px;'>"+
              "          <p> &lt; &gt; "+tools+"</p>"+
              "      </div>"+
              "  </div>"+
@@ -177,19 +160,17 @@ $(document).ready(function(){
 
 
      $.fn.add_event = function(){
-         $('.white-box').on('click',function() {
+         $('.git-link').on('click',function() {
              if ($(this).attr('href') != "undefined"){
-                 // window.location.href = $(this).attr('href');
                  window.open($(this).attr('href'), '_blank');
              }
          });
 
-         $('.white-box').hover(function(){
-           $(this).css("background-color", "#DCDCDC");
-           }, function(){
-           $(this).css("background-color", "#FFFFFF");
+         $('.link-url').on('click',function() {
+             if ($(this).attr('href') != "undefined"){
+                 window.open($(this).attr('href'), '_blank');
+             }
          });
-
     };
 
     $.fn.get_how_many_per_row = function(){
@@ -200,13 +181,6 @@ $(document).ready(function(){
       }
     }
 
-    $(window).on('resize',function(){
-        new_grid_size = $.fn.get_bootstrap_grid($(window).width());
-        if (new_grid_size != bootstrap_grid_size){
-          bootstrap_grid_size = new_grid_size;
-          $.fn.render_projects();
-        }
-    });
 
     $.fn.render_projects = function(){
       parent = $("#projects-container");
@@ -227,7 +201,27 @@ $(document).ready(function(){
       }
     }
 
-    $.fn.render_projects();
-    $.fn.add_event();
+
+
+
+    var pageready = (function(){
+        var thispage = {};
+        thispage.init = function(){
+
+          $(window).on('resize',function(){
+              new_grid_size = $.fn.get_bootstrap_grid($(window).width());
+              if (new_grid_size != bootstrap_grid_size){
+                bootstrap_grid_size = new_grid_size;
+                $.fn.render_projects();
+              }
+          });
+
+          $.fn.render_projects();
+          $.fn.add_event();
+        };
+        return thispage;
+    })();
+
+    pageready.init();
 
 });
